1747ec7989751ca34245e5478a5774b768fa1ce2
lorenuars19 <lorenuar.s19@gmail.com>
2020-04-30 18:04:55 +0200

    Init class : NoissMaker

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,29 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
+  static const String _title = 'Make Some Noiss';
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(_title),
+        ),
+        body: NoissMaker(),
       ),
     );
+  }
+}
+
+class NoissMaker extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO
+    return null;
   }
 }
 
