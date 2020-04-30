618d33f44bb1a4756c793e228f8ad304b48ca88c
Artygo8 <you@example.com>
2020-04-30 20:38:41 +0200

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,21 +9,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: ThemeData(brightness: Brightness.dark),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text(_title),
-        ),
-        body: NoissMaker(),
-      ),
+          appBar: AppBar(title: const Text(_title)), body: NoissMaker()),
     );
   }
 }
 
 class NoissMaker extends StatelessWidget {
-
   static const String _buttonText = 'Make Some Noiss';
   static const double _iconSize = 32;
   @override
@@ -32,21 +27,23 @@ class NoissMaker extends StatelessWidget {
           color: Colors.purpleAccent,
           shape: CircleBorder(),
         ),
-      child: IconButton(
-        alignment: Alignment.center,
-         icon: const Icon(Icons.volume_up, size: _iconSize),
-         color: Colors.white,
-         tooltip: _buttonText,
-         onPressed: () {
-           makeNoiss();
-         }
-         ),
+        child: IconButton(
+            alignment: Alignment.center,
+            icon: const Icon(Icons.volume_up, size: _iconSize),
+            color: Colors.white,
+            tooltip: _buttonText,
+            onPressed: () {
+              makeNoiss();
+            }),
       ),
     );
   }
 
   void makeNoiss() {
-    // TODO make some noiss
+    Future<AudioPlayer> playLocalAsset() async {
+        AudioCache cache = new AudioCache();
+        return await cache.play("hello.mp3");
+    }
+    playLocalAsset();
   }
 }
-
